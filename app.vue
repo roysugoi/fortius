b29cfbc6be3775bcc79f61fd125c7fea71b2ec89
roysugoi <r.vegasjimenez@gmail.com>
2025-01-28 02:19:14 +0100

    tailwind installed - changed text test

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-6">
     <div class="p-6 bg-white dark:bg-gray-800 rounded-lg ring shadow-xl ring-gray-900/5">
-      <h3 class="text-gray-900 dark:text-white text-base font-medium tracking-tight ">Dark or light mode</h3>
+      <h3 class="text-gray-900 dark:text-white text-base font-medium tracking-tight ">Tailwind installed</h3>
       <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-        Change your scheme, this should change.
+        Dark and light mode test.
       </p>
     </div>
   </div>
